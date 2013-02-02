42fc9300be2f3e5af29e76f0501508262e220b98
rstanic12 <rstanic12@gmail.com>
2013-02-02 21:55:02 +0100

    Start

---
+++ net/minecraft/src/GuiElectricFurnace.java
@@ -0,0 +1,46 @@
+package net.minecraft.src;
+
+import org.lwjgl.opengl.GL11;
+
+public class GuiElectricFurnace extends GuiContainer
+{
+    private TileEntityElectricFurnace furnaceInventory;
+
+    public GuiElectricFurnace(InventoryPlayer par1InventoryPlayer, TileEntityElectricFurnace par2TileEntityElectricFurnace)
+    {
+        super(new ContainerElectricFurnace(par1InventoryPlayer, par2TileEntityElectricFurnace));
+        this.furnaceInventory = par2TileEntityElectricFurnace;
+    }
+
+    /**
+     * Draw the foreground layer for the GuiContainer (everything in front of the items)
+     */
+    protected void drawGuiContainerForegroundLayer(int par1, int par2)
+    {
+        this.fontRenderer.drawString(StatCollector.translateToLocal("container.electricFurnace"), 60, 6, 4210752);
+        this.fontRenderer.drawString(StatCollector.translateToLocal("container.inventory"), 8, this.ySize - 96 + 2, 4210752);
+    }
+
+    /**
+     * Draw the background layer for the GuiContainer (everything behind the items)
+     */
+    protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
+    {
+        int var4 = this.mc.renderEngine.getTexture("/gui/furnace.png");
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        this.mc.renderEngine.bindTexture(var4);
+        int var5 = (this.width - this.xSize) / 2;
+        int var6 = (this.height - this.ySize) / 2;
+        this.drawTexturedModalRect(var5, var6, 0, 0, this.xSize, this.ySize);
+        int var7;
+
+        if (this.furnaceInventory.isBurning())
+        {
+            var7 = this.furnaceInventory.getBurnTimeRemainingScaled(12);
+            this.drawTexturedModalRect(var5 + 56, var6 + 36 + 12 - var7, 176, 12 - var7, 14, var7 + 2);
+        }
+
+        var7 = this.furnaceInventory.getCookProgressScaled(24);
+        this.drawTexturedModalRect(var5 + 79, var6 + 34, 176, 14, var7 + 1, 16);
+    }
+}
