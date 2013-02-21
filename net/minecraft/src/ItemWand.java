76ca556197791bb20b78b7defba87fa82fc7d90a
rstanic12 <rstanic12@gmail.com>
2013-02-21 18:55:03 +0100

    Create ItemWand.java
    Add ItemWand (WIP)

---
+++ net/minecraft/src/ItemWand.java
@@ -0,0 +1,16 @@
+package net.minecraft.src;
+
+public class ItemWand extends Item
+{
+  public ItemWand(int par1)
+  {
+    super(par1);
+    this.maxStackSize = 1;
+    this.setCreativeTab(CreativeTabs.tabMisc);
+  }
+
+  public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+  {
+  
+  }
+}
